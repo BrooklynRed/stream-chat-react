23c6f984aef4a69be5f898e6d1a667761c452df4
Alvin Chan <aschan@gmail.com>
2020-05-05 18:58:41 -0700

    Remove empty channel message

--- src/components/EmptyStateIndicator.js
+++ src/components/EmptyStateIndicator.js
@@ -2,11 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withTranslationContext } from '../context';
 
-export const EmptyStateIndicator = withTranslationContext(({ listType, t }) => {
+export const EmptyStateIndicator = withTranslationContext(({ listType }) => {
   let Indicator;
   switch (listType) {
     case 'channel':
-      Indicator = <p>{t('You have no channels currently')}</p>;
+      // Indicator = <p>{t('You have no channels currently')}</p>;
+      Indicator = null;
       break;
     case 'message':
       Indicator = null;
